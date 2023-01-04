cd7dafc6c6d183464f036d0a4ac88118f18df597
Itamar Holder <iholder@redhat.com>
2023-01-04 12:39:49 +0200

    Add a basic start point to get an informer
    Signed-off-by: Itamar Holder <iholder@redhat.com>

--- informers.go
+++ informers.go
@@ -1,1 +1,36 @@
 package mass_machine_type_transition
+
+import (
+	k8sv1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/tools/cache"
+	k6tv1 "kubevirt.io/api/core/v1"
+	"kubevirt.io/client-go/kubecli"
+	"time"
+)
+
+func getVirtCli() (kubecli.KubevirtClient, error) {
+	clientConfig, err := kubecli.GetKubevirtClientConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	virtCli, err := kubecli.GetKubevirtClientFromRESTConfig(clientConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return virtCli, err
+}
+
+func getVmiInformer() (cache.SharedIndexInformer, error) {
+	virtCli, err := getVirtCli()
+	if err != nil {
+		return nil, err
+	}
+
+	listWatcher := cache.NewListWatchFromClient(virtCli.RestClient(), "virtualmachineinstances", k8sv1.NamespaceAll, fields.Everything())
+	vmiInformer := cache.NewSharedIndexInformer(listWatcher, &k6tv1.VirtualMachineInstance{}, 1*time.Hour, cache.Indexers{})
+
+	return vmiInformer, nil
+}
--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package mass_machine_type_transition
 import "os"
 
 func main() {
+	vmiInformer, err := getVmiInformer()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	if vmiInformer != nil {
+		os.Exit(1)
+	}
+
 	os.Exit(0)
 }

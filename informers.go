763ec774ded4b2a9ca0f481a77d95a84667c071a
prnaraya <prnaraya@redhat.com>
2023-05-04 13:59:03 -0500

    WIP commit, adding the base functions for the job and the informers
    Signed-off-by: prnaraya <prnaraya@redhat.com>

--- informers.go
+++ informers.go
@@ -1,12 +1,16 @@
 package mass_machine_type_transition
 
 import (
+	"context"
+	"fmt"
+	"time"
+	
 	k8sv1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/cache"
 	k6tv1 "kubevirt.io/api/core/v1"
 	"kubevirt.io/client-go/kubecli"
-	"time"
 )
 
 func getVirtCli() (kubecli.KubevirtClient, error) {
@@ -23,14 +27,76 @@ func getVirtCli() (kubecli.KubevirtClient, error) {
 	return virtCli, err
 }
 
-func getVmiInformer() (cache.SharedIndexInformer, error) {
-	virtCli, err := getVirtCli()
-	if err != nil {
-		return nil, err
-	}
-
+func getVmiInformer(virtCli kubecli.KubevirtClient) (cache.SharedIndexInformer, error) {
 	listWatcher := cache.NewListWatchFromClient(virtCli.RestClient(), "virtualmachineinstances", k8sv1.NamespaceAll, fields.Everything())
 	vmiInformer := cache.NewSharedIndexInformer(listWatcher, &k6tv1.VirtualMachineInstance{}, 1*time.Hour, cache.Indexers{})
+	
+	vmiInformer.AddEventHandler(cache.ResourceEventHandlerFuncs {
+		DeleteFunc: removeWarningLabel,
+	})
 
 	return vmiInformer, nil
 }
+
+// this is a temporary function that performs the mass machine type transition by manually iterating through each vm and changing the spec
+func updateMachineTypes(virtCli kubecli.KubevirtClient) error {
+	updateMachineType := fmt.Sprint(`{"spec":{"template":{"domain":{"machine":{"type": "pc-q35-rhel9.0.0"}}}}}`)
+	vmList, err := virtCli.VirtualMachine(k8sv1.NamespaceAll).List(&k8sv1.ListOptions{})
+	if err != nil {
+		return err
+	}
+	for _, vm := range vmList.Items {
+		_, err = virtCli.VirtualMachine(vm.Namespace).Patch(vm.Name, types.StrategicMergePatchType, []byte(updateMachineType), &k8sv1.PatchOptions{})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func removeWarningLabel(obj interface{}) {
+	vmi, ok := obj.(*k6tv1.VirtualMachineInstance)
+	if !ok {
+		return
+	}
+	virtCli, err := getVirtCli()
+	if err != nil {
+		return
+	}
+	
+	removeLabel := fmt.Sprint(`{"op": "remove", "path": "metadata/labels/restart-vm-required"}`)
+	virtCli.CoreV1().Nodes().Patch(context.Background(), vmi.Status.NodeName, types.JSONPatchType, []byte(removeLabel), k8sv1.PatchOptions{})
+}
+
+// basic outline for the job, this currently creates a single pod and runs once
+/*
+func machineTypeTransitionJob(virtCli kubecli.KubevirtClient, namespace string) error {
+	jobs := virtCli.BatchV1().Jobs("default")
+	jobSpec := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mass-machine-type-transition"
+			Namespace: "default",
+		},
+		Spec: batchv1.JobSpec{
+			Template: v1.PodTemplateSpec{
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name:    "mass-machine-type-transition",
+							Image:   *image,
+							Command: ["kubectl"],
+							Args:    ["patch", "virtualmachine", "testvm", "--type", "merge", "--patch", '`{"spec":{"template":{"domain":{"machine":{"type": "pc-q35-rhel9.0.0"}}}}}`']
+						},
+					},
+					RestartPolicy: v1.RestartPolicyNever,
+				},
+			},
+		},
+	}
+	
+	_, err := jobs.Create(context.TODO(), jobSpec, &k8sv1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+	return nil
+}*/
--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package mass_machine_type_transition
 import "os"
 
 func main() {
-	vmiInformer, err := getVmiInformer()
+	virtCli, err := getVirtCli()
+	if err != nil {
+		os.Exit(1)
+	}
+	
+	/*vmiInformer, err := getVmiInformer(virtCli)
 	if err != nil {
 		os.Exit(1)
 	}
 
 	if vmiInformer != nil {
 		os.Exit(1)
-	}
+	}*/
 
+	updateMachineTypes(virtCli)
 	os.Exit(0)
 }

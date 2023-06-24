2134e59882f622a56320472c8f730792be2eaead
prnaraya <prnaraya@redhat.com>
2023-06-23 20:18:03 -0500

    Reorganized update_machine_type.go to move some logic from updateMachineType to patchVmMachineType. This was both for ease of testing and general reorganization. I also fleshed out most of the tests for patchVmMachineType and addWarningLabel
    Signed-off-by: prnaraya <prnaraya@redhat.com>

--- update_machine_type.go
+++ update_machine_type.go
@@ -31,31 +31,42 @@ var (
 )
 
 func patchVmMachineType(virtCli kubecli.KubevirtClient, vm *k6tv1.VirtualMachine, machineType string) error {
+	machineTypeSubstrings := strings.Split(machineType, "-")
+		
+	if len(machineTypeSubstrings) != 1 && len(machineTypeSubstrings) != 3 {
+		return nil
+	}
+	
+	// in the case where q35 is the machine type, the VMI status will contain the
+	// full machine type string. Since q35 is an alias for the most recent machine type,
+	// if a VM hasn't been restarted and thus still has an outdated machine type, it should
+	// be updated to the most recent machine type. I'm not able to access the machine type from
+	// VMI status using the kv library, so instead I am opting to update the machine types of all
+	// VMs with q35 instead. It might only be necessary to update the ones that are running as well,
+	// though I am not sure.
+		
+	if len(machineTypeSubstrings) == 1 {
+		if machineTypeSubstrings[0] == "q35" {
+			machineType = "pc-q35-" + latestMachineTypeVersion
+		} else {
+			return nil
+		}
+	}
+	
+	if len(machineTypeSubstrings) == 3 {
+		version := machineTypeSubstrings[2]
+		if strings.Contains(version, "rhel") && version < minimumSupportedMachineTypeVersion {
+			machineTypeSubstrings[2] = latestMachineTypeVersion
+			machineType = strings.Join(machineTypeSubstrings, "-")
+		} else {
+			return nil
+		}
+	}
+	
 	updateMachineType := fmt.Sprintf(`{"spec": {"template": {"spec": {"domain": {"machine": {"type": "%s"}}}}}}`, machineType)
 		
 	_, err := virtCli.VirtualMachine(vm.Namespace).Patch(vm.Name, types.StrategicMergePatchType, []byte(updateMachineType), &k8sv1.PatchOptions{})
-	if err != nil {
-		return err
-	}
-	
-	// add label to running VMs that a restart is required for change to take place
-	if *vm.Spec.Running {
-		// adding the warning label to the VMs regardless if we restart them now or if the user does it manually
-		// shouldn't matter, since the deletion of the VMI will remove the label and remove the vmi list anyway
-		err = addWarningLabel(virtCli, vm)
-		if err != nil {
-			return err
-		}
-		
-		if restartNow {
-			err = virtCli.VirtualMachine(vm.Namespace).Restart(vm.Name, &k6tv1.RestartOptions{})
-			if err != nil {
-				return err
-			}
-		}
-	}
-	
-	return nil
+	return err
 }
 
 func updateMachineTypes(virtCli kubecli.KubevirtClient) error {
@@ -63,31 +74,28 @@ func updateMachineTypes(virtCli kubecli.KubevirtClient) error {
 	if err != nil {
 		return err
 	}
+	
 	for _, vm := range vmList.Items {
 		machineType := vm.Spec.Template.Spec.Domain.Machine.Type
-		machineTypeSubstrings := strings.Split(machineType, "-")
+		err = patchVmMachineType(virtCli, &vm, machineType)
+		if err != nil {
+			fmt.Print(err)
+		}
 		
-		// in the case where q35 is the machine type, the VMI status will contain the
-		// full machine type string. Since q35 is an alias for the most recent machine type,
-		// if a VM hasn't been restarted and thus still has an outdated machine type, it should
-		// be updated to the most recent machine type. I'm not able to access the machine type from
-		// VMI status using the kv library, so instead I am opting to update the machine types of all
-		// VMs with q35 instead. It might only be necessary to update the ones that are running as well,
-		// though I am not sure.
-		
-		if len(machineTypeSubstrings) == 1 {
-			if machineTypeSubstrings[0] == "q35" {
-				machineType = "pc-q35-" + latestMachineTypeVersion
-				return patchVmMachineType(virtCli, &vm, machineType)
+		// add label to running VMs that a restart is required for change to take place
+		if *vm.Spec.Running {
+			// adding the warning label to the VMs regardless if we restart them now or if the user does it manually
+			// shouldn't matter, since the deletion of the VMI will remove the label and remove the vmi list anyway
+			err = addWarningLabel(virtCli, &vm)
+			if err != nil {
+				fmt.Print(err)
 			}
 		}
 		
-		if len(machineTypeSubstrings) == 3 {
-			version := machineTypeSubstrings[2]
-			if strings.Contains(version, "rhel") && version < minimumSupportedMachineTypeVersion {
-				machineTypeSubstrings[2] = latestMachineTypeVersion
-				machineType = strings.Join(machineTypeSubstrings, "-")
-				return patchVmMachineType(virtCli, &vm, machineType)
+		if restartNow {
+			err = virtCli.VirtualMachine(vm.Namespace).Restart(vm.Name, &k6tv1.RestartOptions{})
+			if err != nil {
+				fmt.Print(err)
 			}
 		}
 	}
@@ -95,8 +103,8 @@ func updateMachineTypes(virtCli kubecli.KubevirtClient) error {
 }
 
 func addWarningLabel (virtCli kubecli.KubevirtClient, vm *k6tv1.VirtualMachine) error {
-	addLabel := fmt.Sprint(`{"op": "add", "path": "/metadata/labels", "value" : {"restart-vm-required": "true"}}`)
-	_, err := virtCli.VirtualMachine(vm.Namespace).Patch(vm.Name, types.JSONPatchType, []byte(addLabel), &k8sv1.PatchOptions{})
+	addLabel := fmt.Sprint(`{"metadata": {"labels": {"restart-vm-required": "true"}}}}}`)
+	_, err := virtCli.VirtualMachine(vm.Namespace).Patch(vm.Name, types.StrategicMergePatchType, []byte(addLabel), &k8sv1.PatchOptions{})
 	if err != nil {
 		return err
 	}
--- update_machine_type_test.go
+++ update_machine_type_test.go
@@ -2,16 +2,18 @@ package mass_machine_type_transition
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	
+
 	k8sv1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/cache"
 	k6tv1 "kubevirt.io/api/core/v1"
 	"kubevirt.io/client-go/kubecli"
-//	"kubevirt.io/kubevirt/pkg/testutils"
+	//"kubevirt.io/kubevirt/pkg/testutils"
 )
 
 /*
@@ -42,59 +44,71 @@ var _ = Describe("Update Machine Type", func() {
 			vmInterface = kubecli.NewMockVirtualMachineInterface(ctrl)
 			virtClient.EXPECT().VirtualMachineInstance(k8sv1.NamespaceDefault).Return(vmiInterface).AnyTimes()
 			virtClient.EXPECT().VirtualMachine(k8sv1.NamespaceDefault).Return(vmInterface).AnyTimes()
-		})
-	
-		Describe("AddWarningLabel", func() {
-			
-			It("should apply warning label to VM", func() {
-				vm := NewVMWithMachineType("q35", false)
-				vm.Labels = make(map[string]string)
-				
-				addLabel := fmt.Sprint(`{"op": "add", "path": "/metadata/labels", "value" : {"restart-vm-required": "true"}}`)
-				vmInterface.EXPECT().Patch(vm.Name, types.JSONPatchType, []byte(addLabel), &k8sv1.PatchOptions{}).Return(vm, nil)
-				
-				err := addWarningLabel(virtClient, vm)
-				Expect(err).ToNot(HaveOccurred())
-				
-				vmInterface.EXPECT().Get(vm.Name, &k8sv1.GetOptions{}).Return(vm, nil)
-				vm, err = virtClient.VirtualMachine(vm.Namespace).Get(vm.Name, &k8sv1.GetOptions{})
-				
-				Expect(vm.Labels).To(HaveKeyWithValue("restart-vm-required", "true"))
-			})
-				
-			It("should add VM key to list of VMIs that must be restarted", func() {
-				vm := NewVMWithMachineType("q35", false)
-				addLabel := fmt.Sprint(`{"op": "add", "path": "/metadata/labels", "value" : {"restart-vm-required": "true"}}`)
-				vmInterface.EXPECT().Patch(vm.Name, types.JSONPatchType, []byte(addLabel), &k8sv1.PatchOptions{}).Return(vm, nil)
-				
-				err := addWarningLabel(virtClient, vm)
-				Expect(err).ToNot(HaveOccurred())
-				
-				vmKey := "default/test-vm-q35"
-				_, inVmiList := vmisPendingUpdate[vmKey]
-				Expect(inVmiList).To(BeTrue())
-			})	
+			vmInterface.EXPECT().List(gomock.Any()).AnyTimes()
 		})
 		
-		/*Describe("UpdateMachineType", func() {
-			
-			DescribeTable("when VM Machine Type is", func(machineType string) {
+		Describe("addWarningLabel", func() {
+
+			It("should add VM Key to list of VMIs that need to be restarted", func() {
+				vm := newVMWithMachineType("pc-q35-rhel9.0.0", true)
+				vm.Labels = map[string]string{}
+				
+				vmInterface.EXPECT().Patch(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Do(func(vmName string, pt types.PatchType, data []byte, patchOpts *k8sv1.PatchOptions, subresources ...string) {
+					vm.Labels["restart-vm-required"] = "true"
+				}).AnyTimes()
+				
+				err := addWarningLabel(virtClient, vm)
+				Expect(err).ToNot(HaveOccurred())
+				vmKey, err := cache.MetaNamespaceKeyFunc(vm)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(vmisPendingUpdate).To(HaveKey(vmKey))
+
+				Expect(vm.Labels).To(HaveKeyWithValue("restart-vm-required", "true"), "VM should have 'restart-vm-required' label")
+			})
+		})
+		
+		Describe("patchVmMachineType", func() {
+		
+			DescribeTable("when machine type is", func(machineType string) {
 				//create mock VM with specified machine type
-				//call 	
+				vm := newVMWithMachineType(machineType, false)
+				parsedMachineType := "q35"
+				if machineType != "q35" {
+					splitMachineType := strings.Split(machineType, "-")
+					parsedMachineType = splitMachineType[2]
+				}
+				updateMachineTypeVersion := "pc-q35-" + latestMachineTypeVersion
+				updateMachineType := fmt.Sprintf(`{"spec": {"template": {"spec": {"domain": {"machine": {"type": "%s"}}}}}}`, updateMachineTypeVersion)
+				
+				//call
+				vmInterface.EXPECT().Patch(vm.Name, types.StrategicMergePatchType, []byte(updateMachineType), &k8sv1.PatchOptions{}).Do(func(vmName string, pt types.PatchType, data []byte, patchOpts *k8sv1.PatchOptions, subresources ...string) {
+					if parsedMachineType == "q35" || parsedMachineType < latestMachineTypeVersion {
+						vm.Spec.Template.Spec.Domain.Machine.Type = updateMachineTypeVersion
+					}
+				}).AnyTimes()
+				
+				err := patchVmMachineType(virtClient, vm, machineType)
+				Expect(err).ToNot(HaveOccurred())
+				
+				if parsedMachineType >= minimumSupportedMachineTypeVersion {
+					Expect(vm.Spec.Template.Spec.Domain.Machine.Type).To(Equal(machineType))
+				} else {
+					Expect(vm.Spec.Template.Spec.Domain.Machine.Type).To(Equal(updateMachineTypeVersion))
+				}
 			},
-				Entry("q35", "q35"),
-				Entry("pc-q35-rhelx.x.x and less than latest machine type version", "pc-q35-rhel8.2.0"),
-				Entry("pc-q35-rhelx.x.x and greater than or equal to latest machine type version", "pc-q35-rhel9.0.0"),
+				Entry("'q35' should update machine type to latest version", "q35"),
+				Entry("'pc-q35-rhelx.x.x' and less than minimum supported machine type version should update machine type to latest version", "pc-q35-rhel8.2.0"),
+				Entry("'pc-q35-rhelx.x.x' and greater than or equal to latest machine type version should not affect machine type", "pc-q35-rhel9.0.0"),
 			)
-		})*/
+		})
 })
 
-func NewVMWithMachineType(machineType string, running bool) *k6tv1.VirtualMachine {
+func newVMWithMachineType(machineType string, running bool) *k6tv1.VirtualMachine {
 	vmName := "test-vm-" + machineType
 	testVM := &k6tv1.VirtualMachine{
 		ObjectMeta: k8sv1.ObjectMeta{
 			Name: vmName,
-			Namespace: "default",
+			Namespace: k8sv1.NamespaceDefault,
 		},
 		Spec: k6tv1.VirtualMachineSpec{
 			Running: &running,
@@ -109,5 +123,6 @@ func NewVMWithMachineType(machineType string, running bool) *k6tv1.VirtualMachin
 			},
 		},
 	}
+	testVM.Labels = map[string]string{}
 	return testVM
 }
